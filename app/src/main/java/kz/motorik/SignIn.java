3ebba29beb3c5015f051b161a6bc97f3b2f40309
elgondin Ersultan <elgondin Ersultan>
2017-03-20 23:23:27 +0500

    Initial commit

---
+++ app/src/main/java/kz/motorik/SignIn.java
@@ -0,0 +1,13 @@
+package kz.motorik;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
