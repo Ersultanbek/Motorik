95eeec6b304b9d4ba8259d76d9a4c5b9c161993d
MotorikKZ <webar1204@gmail.com>
2017-04-03 10:10:26 +0500

    Sigin

--- app/src/main/java/kz/motorik/StartActivity.java
+++ app/src/main/java/kz/motorik/StartActivity.java
@@ -1,13 +1,32 @@
 package kz.motorik;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends Activity {
+
+public class StartActivity extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        final Intent clientPage = new Intent(this, SignIn.class);
+        Button clientButton = (Button)findViewById(R.id.ClientButton);
+      ///TEXT HERE
+        clientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(clientPage);
+            }});
+
+
     }
+
+
+
 }
