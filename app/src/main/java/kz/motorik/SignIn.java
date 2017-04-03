f1040a1f4101f1716aa2a1b1efd506838952006d
MotorikKZ <webar1204@gmail.com>
2017-04-03 10:08:51 +0500

    Sigin

--- app/src/main/java/kz/motorik/SignIn.java
+++ app/src/main/java/kz/motorik/SignIn.java
@@ -2,12 +2,38 @@ package kz.motorik;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
 
 public class SignIn extends Activity {
 
+
+    private String login="";
+    private String password="";
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        Button signIn = (Button)findViewById(R.id.signIn);
+
+        signIn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView loginField = (TextView)findViewById(R.id.EmailField);
+                TextView passWordField = (TextView)findViewById(R.id.PasswordField);
+
+            }
+        });
     }
+
+
+
+
 }
